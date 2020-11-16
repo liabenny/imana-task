78c7ba96ecc7f10a969022963925c3cd9b84b6c3
Liangbin Zhu <liabenny@Liangbins-MacBook-Pro.local>
2020-11-15 18:58:06 -0800

    deployment test

--- imanatask-web/src/app/web-request.service.ts
+++ imanatask-web/src/app/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://localhost:8080';
+    this.ROOT_URL = 'http://imanatask-core:8080';
   }
 
   get(uri: string, payload: Object) {
